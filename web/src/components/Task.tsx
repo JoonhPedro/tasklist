25412986c75cfc45e68ed9936596d8655c35ac45
João Pedro <joao.veo77@gmail.com>
2023-10-28 15:31:38 -0300

    style: modificação do layout do projeto / .tsx: criação das funcionalidades do projeto

--- web/src/components/Task.tsx
+++ web/src/components/Task.tsx
@@ -1,27 +1,62 @@
 import React, { useState } from 'react'
+
 import Offtask from './Offtask'
+
 const Task: React.FC = () => {
-  const [task, setTask] = useState('')
+  const [list, setList] = useState('')
+  const [tasks, setTasks] = useState<string[]>([])
+
+  const addTask = () => {
+    if (list.trim()) {
+      setTasks([...tasks, list.trim()])
+      setList('')
+    }
+  }
+
+  const removeTask = (index: number) => {
+    const updatedTasks = [...tasks]
+    updatedTasks.splice(index, 1)
+    setTasks(updatedTasks)
+  }
 
   return (
     <>
       <div className="container">
-        <h1>Lista de Tarefa</h1>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault()
+            addTask()
+          }}
+        >
+          <h1>Lista de Tarefa</h1>
+          <input
+            type="text"
+            className="input-task"
+            name="tasklist"
+            placeholder="Digite sua Tarefa"
+            maxLength={35}
+            required
+            value={list}
+            onChange={(e) => setList(e.target.value)}
+          />
+          <button type="submit" className="button-task">
+            Adicionar
+          </button>
+        </form>
 
-        <input
-          type="text"
-          className="input-task"
-          name="tasklist"
-          placeholder="Digite sua Tarefa"
-          maxLength={28}
-          required
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <label htmlFor="nome"></label>
-        <button type="submit" className="button-task">
-          Adicionar
-        </button>
+        <ol>
+          {tasks.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button
+                className="button-delete"
+                onClick={() => removeTask(index)}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ol>
 
         <Offtask />
       </div>
