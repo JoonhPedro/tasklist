c31cea6bb997c5ff8132d7d679015008e5d05aec
João Pedro <joao.veo77@gmail.com>
2023-10-30 19:50:42 -0300

    Documetação do code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import Task from './components/Task'
 function App() {
   return (
     <>
+      {/* Import do component task para o app :) */}
       <Task />
     </>
   )
--- web/src/components/Task.tsx
+++ web/src/components/Task.tsx
@@ -2,32 +2,45 @@ import React, { useState } from 'react'
 import Offtask from './Offtask'
 
 const Task: React.FC = () => {
+  // Estado para controlar o valor do input.
   const [list, setList] = useState('')
+  // E para armazenar a Tasklist.
   const [task, setTask] = useState<string[]>([])
 
+  // Função para adicionar uma nova task a lista.
   const addTask = () => {
+    // Verifica se a string 'list' contém algum conteúdo não vazio.
     if (list.trim()) {
+      // Adiciona a task e impede a adição de uma entrada vazia.
       setTask([...task, list.trim()])
+      // Limpa o valor do input
       setList('')
     }
   }
-
+  // Função que remove a lista de tarefa existente
   const removeTask = (tolist: number) => {
+    // Cria uma cópia da lista de tarefas
     const updatedTasks = [...task]
+    // Remove a tarefa com o índice 'toList'
     updatedTasks.splice(tolist, 1)
+    // Atualiza o estado com a lista de tarefas atualizada
     setTask(updatedTasks)
   }
 
   return (
     <>
       <div className="container">
+        {/* Formulário para adicionar uma nova task */}
         <form
           onSubmit={(event) => {
+            // Impede o comportamento padrão do formulário
             event.preventDefault()
+            // Chama a função para adicionar a task
             addTask()
           }}
         >
           <h1>Lista de Tarefas</h1>
+          {/* Input para digitar a nova task */}
           <input
             type="text"
             className="input-task"
@@ -36,16 +49,20 @@ const Task: React.FC = () => {
             maxLength={30}
             required
             value={list}
+            // Atualiza o estado 'list' com o valor do input
             onChange={(e) => setList(e.target.value)}
           />
+          {/* Botão para adicionar a task */}
           <button type="submit" className="button-task">
             Adicionar
           </button>
         </form>
-        {task.length === 0 ? <Offtask /> : true}
-
+        {/* Exibe o componente Offtask se a lista de tarefas estiver vazia */}
+        {task.length === 0 && <Offtask />}
+        {/* Verifica se tem alguma task na lista e exibe as tasks */}
         {task.length > 0 && (
           <ol className="list-task">
+            {/* Mapeia a lista de tarefas e renderiza cada item o botão de apagar a task */}
             {task.map((todo, tolist) => (
               <li key={tolist}>
                 {todo}
